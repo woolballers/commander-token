8e88407e8c0a14cfccc7b114886feb04e41f6669
tomlightning <le.tomer@gmail.com>
2023-03-12 16:42:33 +0200

    more tests refactor

--- constants/test.ts
+++ constants/test.ts
@@ -1,3 +1,8 @@
 export const TOKEN_NAME = "CommanderTokenTest";
 export const TOKEN_SYMBOL = "CTT";
-export const INITIAL_MINT_COUNT = 3;
+export const INITIAL_MINT_COUNT = 4;
+
+
+export const TOKEN_NAME2 = "CommanderTokenTest2";
+export const TOKEN_SYMBOL2 = "CTT2";
+export const INITIAL_MINT_COUNT2 = 2;
